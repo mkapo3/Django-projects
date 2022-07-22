457cc00f86b50471c21ae7a358f8a8f282443540
Muharem Kapo <mkapo3@etf.unsa.ba>
2022-07-22 17:07:43 +0200

    Echo GET data

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,11 +2,11 @@ import requests
 
 #endpoint = "https://httpbin.org/status/200/"
 #endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint, json={"query":"Hello world!"}) #API -> Method
-print(response.text)
-print(response.status_code)
+response = requests.get(endpoint, params={"abc": 123}, json={"query":"Nesto"})  # API -> Method
+# print(response.text)
+# print(response.status_code)
 
 print(response.json())
 #
