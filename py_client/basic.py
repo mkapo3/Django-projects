0f904f8fd4fdcb7810bdd0834e46ebf11f17afc8
Muharem Kapo <mkapo3@etf.unsa.ba>
2022-07-22 16:43:56 +0200

    Created basic API

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,12 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+#endpoint = "https://httpbin.org/status/200/"
+#endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
 
 response = requests.get(endpoint, json={"query":"Hello world!"}) #API -> Method
-#print(response.text)
+print(response.text)
+print(response.status_code)
 
 print(response.json())
+#
