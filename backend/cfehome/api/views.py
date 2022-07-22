83ad75117fbce75a77fd752b5af7c0c037478911
Muharem Kapo <mkapo3@etf.unsa.ba>
2022-07-22 19:10:37 +0200

    Implemented Product serializer

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,14 +1,21 @@
 import json
-from django.http import JsonResponse, HttpResponse
+#from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.serializers import ProductSerializer
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    
+    #if request.method != "GET":
+        #return Response({"detail": "POST not allowed"}, status=405)
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price'])
-        
-    #return HttpResponse(data, headers={"content-type":"application/json"})
-    return JsonResponse(data)
+    if instance:
+        #data = model_to_dict(model_data, fields=['id','title','price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
